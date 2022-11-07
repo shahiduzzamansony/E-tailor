c3a840f615c2f5822c14929a325bf5baeec976e9
Your shahiduzzamansony <shahiduzzamansony2703@gmail.com>
2022-11-07 22:24:32 +0600

    some components created

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,19 @@
+import Main from "./Layout/Main/Main";
+import Home from "./pages/Home/Home";
+
 const { createBrowserRouter } = require("react-router-dom");
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
 
 export default router;
